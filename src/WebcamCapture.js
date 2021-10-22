9520586f33a016fe7e9782bb1b98a6dc012efc8c
Yasin Khan <yasinkhan500@gmail.com>
2021-10-21 20:05:42 -0700

    prepped for deployment

---
+++ src/WebcamCapture.js
@@ -0,0 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useCallback, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import Webcam from 'react-webcam';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import { useDispatch } from 'react-redux';
+import { setCameraImage } from './features/cameraSlice';
+import './WebcamCapture.css';
+
+const videoConstraints = {
+  width: 250,
+  height: 400,
+  facingMode: 'user',
+};
+
+function WebcamCapture() {
+  const webcamRef = useRef(null);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    dispatch(setCameraImage(imageSrc));
+    history.push('/preview');
+  }, [webcamRef]);
+
+  return (
+    <div className='webcamCapture'>
+      <Webcam
+        audio={false}
+        height={videoConstraints.height}
+        ref={webcamRef}
+        screenshotFormat='image/jpeg'
+        width={videoConstraints.width}
+        videoConstraints={videoConstraints}
+      />
+
+      <RadioButtonUncheckedIcon
+        className='webcamCapture__button'
+        onClick={capture}
+      />
+    </div>
+  );
+}
+
+export default WebcamCapture;
